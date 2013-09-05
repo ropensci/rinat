419d7201ed3416d87a51dc56c2ec553bfd57070a
Edmund Hart <edmund.m.hart@gmail.com>
2013-09-05 13:27:57 -0600

    Completed basic get obs funcion
    Opted to implement this with CSV calls over JSON calls because it was
    easier to collate results.

--- R/get_obs_inat.R
+++ R/get_obs_inat.R
@@ -1,8 +1,9 @@
 #' get_obs_inat - Primary function to retrive observations from iNaturalist
 #' @param query Query string
-#' @param maxresults - 
+#' @param maxresults the maximum number of results to return
+#' @return a dataframe of the number of observations requestsed
 #' @examples \dontrun{
-#'   get_obs(query="Monarch Butterfly")
+#'   get_obs_inat(query="Monarch Butterfly")
 #' }
 #' @import httr plyr
 #' @export
@@ -12,26 +13,39 @@
 get_obs_inat <- function(query=NULL,maxresults=100) 
 {  
   base_url <- "http://www.inaturalist.org/"
-  q_path <- "observations.json"
+  q_path <- "observations.csv"
   search <- paste("&q=",gsub(" ","+",query),sep="")
-  data_list <- list()
+  ping_path <- "observations.json"
+  ping_query <- paste(search,"&per_page=1&page=1",sep="")
   ### Make the first ping to the server to get the number of results
-  if(maxresults > 200){
+  ### easier to pull down if you make the query in json, but easier to arrange results
+  ### that come down in CSV format
+  ping <-  GET(base_url,path = ping_path, query = ping_query)
+  total_res <- as.numeric(ping$headers$`x-total-entries`)
+  
+  if(total_res == 0){
+    stop("Your search returned zero results.  Either your species of interest has no records or you entered an invalid search")
+  }
+  
+  
+  
     page_query <- paste(search,"&per_page=200&page=1",sep="")
     data <-  GET(base_url,path = q_path, query = page_query)
-    total_res <- as.numeric(data$headers$`x-total-entries`)
-    ### add rnat::
-    data_list[[1]] <- fix_df(ldply(content(data,as = "parsed"),flatten_list))
+    data_out <-read.csv(textConnection(content(data)))
+  if(maxresults > 200){
       for(i in 2:ceiling(total_res/200)){
         page_query <- paste(search,"&per_page=200&page=",i,sep="")
         data <-  GET(base_url,path = q_path, query = page_query)
-        ### add rnat::
-        data_list[[i]] <- fix_df(ldply(content(data,as = "parsed"),flatten_list))
+        data_out <- rbind(data_out, read.csv(textConnection(content(data))))
       }
     
-  
   }
   
- 
-  return(out)
+  if(maxresults < dim(data_out)[1]){
+    data_out <- data_out[1:maxresults,]
+  }
+  
+  return(data_out)
 }
+
+
