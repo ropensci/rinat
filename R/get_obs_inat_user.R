eb05be940d0157343f92be26a6a1d40e3eb4d72f
Edmund Hart <edmund.m.hart@gmail.com>
2013-10-01 16:31:34 -0600

    added a couple more searh routines
    Finishing up all the options that you can get data with.

--- R/get_obs_inat_user.R
+++ R/get_obs_inat_user.R
@@ -1,21 +1,19 @@
-#' get_obs_inat - Primary function to retrive observations from iNaturalist
-#' @param query Query string
-#' @param maxresults the maximum number of results to return
-#' @return a dataframe of the number of observations requestsed
+#' Get all the observations of a specific user
+#' @param id a single id for an inaturalist observation record
+#' @return a list with full details on a given record
 #' @examples \dontrun{
-#'   get_obs_inat(query="Monarch Butterfly")
+#'   m_obs <- get_obs_inat(query="Monarch Butterfly")
+#'   get_obs_inat_id(m_obs$Id[1])
 #' }
 #' @import httr plyr
 #' @export
 
 
-
-get_obs_inat <- function(query=NULL,maxresults=100) 
-{  
+### Needs more work, there can be 1000's of results, need to implement pagination similar to the main observation query method.
+get_obs_inat_user <- function(username){
   
   base_url <- "http://www.inaturalist.org/"
   q_path <- "observations.csv"
-  search <- paste("&q=",gsub(" ","+",query),sep="")
   ping_path <- "observations.json"
   ping_query <- paste(search,"&per_page=1&page=1",sep="")
   ### Make the first ping to the server to get the number of results
@@ -30,15 +28,15 @@ get_obs_inat <- function(query=NULL,maxresults=100)
   
   
   
-    page_query <- paste(search,"&per_page=200&page=1",sep="")
-    data <-  GET(base_url,path = q_path, query = page_query)
-    data_out <-read.csv(textConnection(content(data)))
+  page_query <- paste(search,"&per_page=200&page=1",sep="")
+  data <-  GET(base_url,path = q_path, query = page_query)
+  data_out <-read.csv(textConnection(content(data)))
   if(maxresults > 200){
-      for(i in 2:ceiling(total_res/200)){
-        page_query <- paste(search,"&per_page=200&page=",i,sep="")
-        data <-  GET(base_url,path = q_path, query = page_query)
-        data_out <- rbind(data_out, read.csv(textConnection(content(data))))
-      }
+    for(i in 2:ceiling(total_res/200)){
+      page_query <- paste(search,"&per_page=200&page=",i,sep="")
+      data <-  GET(base_url,path = q_path, query = page_query)
+      data_out <- rbind(data_out, read.csv(textConnection(content(data))))
+    }
     
   }
   
@@ -48,5 +46,3 @@ get_obs_inat <- function(query=NULL,maxresults=100)
   
   return(data_out)
 }
-
-
