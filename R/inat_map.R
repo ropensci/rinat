59a39f25424bd51b7fb03e18fb92f3d436cfef94
Edmund Hart <edmund.m.hart@gmail.com>
2013-10-27 22:39:01 -0700

    Added simple mapping functions, closes issue #4

---
+++ R/inat_map.R
@@ -0,0 +1,31 @@
+#' Plot iNaturalist observations 
+#' 
+#' @description Plot observations from iNaturalist.  You have the option of automatically plotting, or returning a ggplot map object that you can add layers on to.
+#' @param data data frame of iNaturalist observations
+#' @param map the map region to plot, you can find full documentation in the \code{\link{map}} package, default is usa.
+#' @param subregion the name of the subregion to plot, see full documentation in the \code{\link{map}} package
+#' @param plot a TRUE or FALSE variable, TRUE plots the map object and returns it, and FALSE returns a ggplot2 object that you can modify and plot later
+#' 
+#' @return a ggplot2 map object
+#' @examples \dontrun{
+#'   m_obs <- get_obs_inat(taxon="Ambystoma maculatum")
+#'   salamander_map <- inat_map(m_obs,plot=FALSE)
+#'   ### Now we can modify the returned map
+#'   salamander_map + borders("state") + theme_bw()
+#' }
+#' @import map ggplot2
+#' @export
+
+inat_map <- function(data, map = "usa",subregion=".",plot=TRUE){
+  map_df <- map_data(map,region=subregion)
+  base_map <- ggplot(map.df,aes(x=long,y=lat)) + geom_polygon(aes(group=group), fill="white", color="gray40", size=0.2)
+  sp_map <- base_map + geom_point(data=data,aes(x=Longitude,y=Latitude))
+  
+  if(plot){
+    print(sp_map)
+    return(sp_map)
+  } else {
+    return(sp_map)
+  }
+  
+}
