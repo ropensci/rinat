196d6d4a3a1b677a50abdf2121a1ccada46fb92c
Edmund Hart <edmund.m.hart@gmail.com>
2013-11-11 22:16:39 -0800

    Added function to get taxon stats
    close  #2

--- R/get_inat_taxon_stats.R
+++ R/get_inat_taxon_stats.R
@@ -4,9 +4,10 @@
 #' @param date retrieve observations on a specific date, must be a string in the form YYYY-MM-DD
 #' @param date_range a vector of dates, in the form YYYY-MM-DD
 #' @param place get taxon stats by place, you can find place id's on the iNaturalist page: http://www.inaturalist.org/places, must be a numeric ID
-#' @param project get taxon stats by project id, 
+#' @param project get taxon stats by project id
+#' @param uid get taxon stats by user id 
 #' 
-#' @return a ggplot2 map object
+#' @return a dataframe with search results
 #' @examples \dontrun{
 #'   m_obs <- get_obs_inat(taxon="Ambystoma maculatum")
 #'   salamander_map <- inat_map(m_obs,plot=FALSE)
@@ -15,3 +16,29 @@
 #' }
 #' @import map ggplot2
 #' @export
+
+
+get_inat_taxon_stats(date = NULL, date_range = NULL, place = NULL, project = NULL, uid=NULL){
+  base_url <- "http://www.inaturalist.org/"
+  q_path <- "observations/taxon_stats.json"
+  search = ""
+  if(!is.null(date)){
+    search = paste(search,"&on=",date,sep="")
+  }
+  if(!is.null(date_range)){
+    search = paste(search,"d1=",date_range[1],"&d2=",date_range[2],sep="")
+  }
+  if(!is.null(place)){
+    search = paste(search,"place_id=",place, sep="")
+  }
+  if(!is.null(project)){
+    search = paste(search,"projects=",project,sep="")
+  }
+  if(!is.null(uid)){
+    search = paste(search,"&user_id=",uid,sep="")
+  }
+  
+  data <-  content(GET(base_url,path = q_path, query = search))
+  return(data)
+}
+
