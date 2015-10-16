8b8473eb9672009d17f25b18ccd4e6805c82084a
Scott Chamberlain <myrmecocystus@gmail.com>
2015-10-16 09:25:47 -0700

    updated man files for new roxygen2 version, some tidying

--- R/get_inat_obs_id.R
+++ R/get_inat_obs_id.R
@@ -1,19 +1,16 @@
 #' Get information on a specific observation
+#'
+#' @export
+#' @import httr plyr jsonlite
 #' @param id a single id for an inaturalist observation record
 #' @return a list with full details on a given record
 #' @examples \dontrun{
-#'   m_obs <- get_inat_obs(query="Monarch Butterfly")
-#'   get_inat_obs_id(m_obs$Id[1])
+#' m_obs <- get_inat_obs(query="Monarch Butterfly")
+#' get_inat_obs_id(m_obs$id[1])
 #' }
-#' @import httr plyr jsonlite
-#' @export
-
 get_inat_obs_id <- function(id){
   base_url <- "http://www.inaturalist.org/"
-  q_path <- paste("observations/",as.character(id),".json",sep="")
-  id_info <-  fromJSON(content(GET(base_url,path = q_path), as = "text"))
-  ## convert to string
-
-  return(id_info)
+  q_path <- paste("observations/",as.character(id),".json", sep = "")
+  fromJSON(content(GET(base_url,path = q_path), as = "text"))
 }
 
--- R/inat_map.R
+++ R/inat_map.R
@@ -1,11 +1,11 @@
-#' Plot iNaturalist observations 
-#' 
+#' Plot iNaturalist observations
+#'
 #' @description Plot observations from iNaturalist.  You have the option of automatically plotting, or returning a ggplot map object that you can add layers on to.
 #' @param data data frame of iNaturalist observations
 #' @param map the map region to plot, you can find full documentation in the \code{\link{map}} package, default is usa.
 #' @param subregion the name of the subregion to plot, see full documentation in the \code{\link{map}} package
 #' @param plot a TRUE or FALSE variable, TRUE plots the map object and returns it, and FALSE returns a ggplot2 object that you can modify and plot later
-#' 
+#'
 #' @return a ggplot2 map object
 #' @examples \dontrun{
 #'   m_obs <- get_inat_obs(taxon="Ambystoma maculatum")
@@ -17,15 +17,18 @@
 #' @export
 
 inat_map <- function(data, map = "usa",subregion=".",plot=TRUE){
-  map_df <- map_data(map,region=subregion)
-  base_map <- ggplot(map_df,aes(x=long,y=lat)) + geom_polygon(aes(group=group), fill="white", color="gray40", size=0.2)
-  sp_map <- base_map + geom_point(data=data,aes(x=Longitude,y=Latitude))
-  
+  map_df <- map_data(map, region = subregion)
+  base_map <-
+    ggplot(map_df, aes(x = long, y = lat)) +
+      geom_polygon(aes(group = group), fill="white", color="gray40", size=0.2)
+  sp_map <- base_map +
+    geom_point(data = data, aes(x = longitude, y = latitude))
+
   if(plot){
     print(sp_map)
     return(sp_map)
   } else {
     return(sp_map)
   }
-  
+
 }
